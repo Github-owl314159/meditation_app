4330999709118d9ac3b86268157f7873308c7c21
Rollo <sirrollodev@gmail.com>
2019-04-17 12:20:01 +0200

    progress

---
+++ app.js
@@ -0,0 +1,42 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+  const timeButtonArray = document.querySelectorAll(".time-select button");
+  const soundButtonArray = document.querySelectorAll(".sound-picker button");
+
+  const outlineLength = outline.getTotalLength();
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsedTime = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsedTime % 60);
+    let minutes = Math.floor(elapsedTime / 60);
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+  };
+};
+
+app();
